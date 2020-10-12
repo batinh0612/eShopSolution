2ccb48087013fc1489d302053ebc998624775f69
TinhNB <tinhnb@miichisoft.com>
2020-10-12 10:45:21 +0700

    TinhNB commit

--- eShopSolution.Application/Catalog/Products/ManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -29,7 +29,62 @@ namespace eShopSolution.Application.Catalog.Products
 
         public async Task<int> Delete(int productId)
         {
-            throw new NotImplementedException();
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new EShopException($"Cannot find a product:  { productId} ");
+            _context.Products.Remove(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Get all paging
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        {
+            //select join
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join c in _context.Categories on pic.CategoryId equals c.Id
+                        where pt.Name.Contains(request.Keyword)
+                        select new { p, pt, pic };
+            //filter
+            if (!string.IsNullOrEmpty(request.Keyword))
+                query = query.Where(x => x.pt.Name.Contains(request.Keyword));
+            if (request.CategoryIds.Count > 0)
+                query = query.Where(p => request.CategoryIds.Contains(p.pic.CategoryId));
+
+
+            //paging
+            int totalRow = await query.CountAsync();
+            var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(x => new ProductViewModel()
+                {
+                    Id = x.p.Id,
+                    Name = x.pt.Name,
+                    CreatedDate = x.p.CreatedDate,
+                    Description = x.pt.Description,
+                    Details = x.pt.Details,
+                    LanguageId = x.pt.LanguageId,
+                    OriginalPrice = x.p.OriginalPrice,
+                    Price = x.p.Price,
+                    SeoAlias = x.pt.SeoAlias,
+                    SeoDescription = x.pt.SeoDescription,
+                    SeoTitle = x.pt.SeoTitle,
+                    Stock = x.p.Stock,
+                    ViewCount = x.p.ViewCount
+                }).ToListAsync();
+
+            //select and projection
+            var pagedResult = new PagedResult<ProductViewModel>()
+            {
+                TotalRecord = totalRow,
+                Items = data
+            };
+
+            return pagedResult;
         }
 
         public async Task<List<ProductViewModel>> GetAll()
